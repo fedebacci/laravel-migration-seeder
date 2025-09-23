434613923e25d2a9d1dfce8621573d4ae790e837
fedebacci <federicobacci1999@gmail.com>
2025-09-23 15:48:50 +0200

    basic table for showing data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,31 +6,83 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1>
-                        Home Page
+                    <h1 class="text-center mb-5">
+                        Trains
                     </h1>
 
-                    @foreach ($trains as $train)
-                        <p>
-                            {{ $train->agency }}
-                             - 
-                            {{ $train->departure_station }}
-                             - 
-                            {{ $train->arrival_station }}
-                             - 
-                            {{ $train->departure_time }}
-                             - 
-                            {{ $train->arrival_time }}
-                             - 
-                            {{ $train->train_code }}
-                             - 
-                            {{ $train->carriages }}
-                             - 
-                            {{ $train->in_time ? 'In orario' : 'In ritardo' }}
-                             - 
-                            {{ $train->deleted ? 'Cancellato' : 'Non cancellato' }}
-                        </p>
-                    @endforeach
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr class="table-dark">
+                                <th scope="col">
+                                    Train Code
+                                </th>
+                                <th scope="col">
+                                    Agency
+                                </th>
+                                <th scope="col">
+                                    Departure Station
+                                </th>
+                                <th scope="col">
+                                    Arrival Station
+                                </th>
+                                <th scope="col">
+                                    Departure Time
+                                </th>
+                                <th scope="col">
+                                    Arrival Time
+                                </th>
+                                <th scope="col">
+                                    Carriages
+                                </th>
+                                <th scope="col">
+                                    In Time
+                                </th>
+                                <th scope="col">
+                                    Deleted
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach ($trains as $train)
+                                <tr>
+                                    <td scope="row">
+                                        {{ $train->train_code }}
+                                    </td>
+                                    <td>
+                                        {{ $train->agency }}
+                                    </td>
+                                    <td>
+                                        {{ $train->departure_station }}
+                                    </td>
+                                    <td>
+                                        {{ $train->arrival_station }}
+                                    </td>
+                                    <td>
+                                        {{ $train->departure_time }}
+                                    </td>
+                                    <td>
+                                        {{ $train->arrival_time }}
+                                    </td>
+                                    <td>
+                                        {{ $train->carriages }}
+                                    </td>
+                                    <td>
+                                        {{ $train->in_time ? 'In orario' : 'In ritardo' }}
+                                    </td>
+                                    <td>
+                                        {{ $train->deleted ? 'Cancellato' : 'Non cancellato' }}
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+
+
+
+
+
                 </div>
             </div>
         </div>
