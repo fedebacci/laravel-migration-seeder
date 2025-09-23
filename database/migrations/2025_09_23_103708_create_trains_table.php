d4cd331bdfa44bdcb19f08439f9c536f71be27bc
fedebacci <federicobacci1999@gmail.com>
2025-09-23 12:53:35 +0200

    Train model creation and migration for trains table creation

---
+++ database/migrations/2025_09_23_103708_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20)->unique();
+            $table->tinyInteger('carriages')->unsigned();
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
